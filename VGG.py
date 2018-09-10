31d6587bfe2502a64939dfb65f1bedec599c8f07
majian <565507114@qq.com>
2018-09-10 19:38:02 +0800

    modify Vgg.py

--- VGG.py
+++ VGG.py
@@ -42,6 +42,5 @@ def VGG16(x, n_class, is_pretrain=True):
         x = tools.FC_layer('fc7', x, out_nodes=4096)
         with tf.name_scope('batch_norm2'):
             x = tools.batch_norm(x)
-        #x = tools.FC_layer('fc8', x, out_nodes=n_class)
-        x = tools.Last_layer('fc8', x, out_nodes = n_class)
+        x = tools.FC_layer('fc8', x, out_nodes=n_class)
         return x
